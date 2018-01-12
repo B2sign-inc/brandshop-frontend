29ce3cc7a23bb921f9b23874b36abbee5c4ee9fc
Ben <ishaobinliang@gmail.com>
2018-01-12 15:46:29 -0800

    add product and category

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { DefaultComponent } from './default/default.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full'}
+  { path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: DefaultComponent },
+      { path: 'categories/:id/products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+import { DefaultComponent } from './default/default.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, CategoryComponent, ProductListComponent, ProductComponent, DefaultComponent]
 })
 export class HomeModule { }
