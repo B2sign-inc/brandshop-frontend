47c6532889203446dab33e4e99536afa6f660b1c
Ben <ishaobinliang@gmail.com>
2018-02-16 15:22:59 -0800

    add order list

--- src/app/user/order/order-list/order-list.component.ts
+++ src/app/user/order/order-list/order-list.component.ts
@@ -10,10 +10,31 @@ export class OrderListComponent implements OnInit {
 
   orders: Order[];
 
+  limit: number = 15;
+  page: number = 1;
+  total: number;
+
   constructor(private orderService: OrderService) { }
 
   ngOnInit() {
-
+    this.load();
   }
 
+  load() {
+    const params = {
+      page: this.page,
+      limit: this.limit,
+    };
+
+    this.orderService.query(params).subscribe(data => {
+      this.orders = data['data'];
+      this.total = data['meta']['total'];
+    })
+  }
+
+  paginatorOnChange($event) {
+    this.limit = $event.pageSize;
+    this.page = $event.pageIndex + 1;
+    this.load();
+  }
 }
--- src/app/user/order/order.module.ts
+++ src/app/user/order/order.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderDetailComponent } from './order-detail/order-detail.component';
 import { OrderListComponent } from './order-list/order-list.component';
+import { MatPaginatorModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatPaginatorModule,
   ],
   declarations: [OrderDetailComponent, OrderListComponent]
 })
